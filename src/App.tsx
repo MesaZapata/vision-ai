fc4544018f53ed9f1e6e5b3a5e787d1fd720e18b
Quintero-0226 <emmanuelquinterogill@gmail.com>
2025-02-21 16:37:42 -0500

    Agregar componente UploadFile en la aplicación

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
+<<<<<<< HEAD
 import './App.css'
 import { ImageCaptioning } from './modules/vision'
+=======
+import "./App.css";
+import { UploadFile } from "./modules/vision";
+>>>>>>> eff078b (Agregar componente UploadFile en la aplicación)
 
 export const App = () => {
-
   return (
     <>
+<<<<<<< HEAD
       <ImageCaptioning />
+=======
+      <UploadFile />
+>>>>>>> eff078b (Agregar componente UploadFile en la aplicación)
     </>
-  )
-}
-
-
+  );
+};
