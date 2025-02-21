eff078b4bade8ff1a638a8077f9b2cd0e24d8492
Quintero-0226 <emmanuelquinterogill@gmail.com>
2025-02-21 16:37:42 -0500

    Agregar componente UploadFile en la aplicación

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import './App.css'
+import "./App.css";
+import { UploadFile } from "./modules/vision";
 
 export const App = () => {
-
   return (
     <>
+      <UploadFile />
     </>
-  )
-}
-
-
+  );
+};
