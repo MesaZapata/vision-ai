53178e521488fc1ecf826756752e4030650f7be1
Quintero-0226 <emmanuelquinterogill@gmail.com>
2025-02-21 16:44:53 -0500

    Revert "Agregar componente UploadFile en la aplicación"
    This reverts commit eff078b4bade8ff1a638a8077f9b2cd0e24d8492.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,13 @@
-<<<<<<< HEAD
-import './App.css'
-import { ImageCaptioning } from './modules/vision'
-=======
 import "./App.css";
 import { UploadFile } from "./modules/vision";
->>>>>>> eff078b (Agregar componente UploadFile en la aplicación)
 
 export const App = () => {
+
   return (
     <>
-<<<<<<< HEAD
-      <ImageCaptioning />
-=======
       <UploadFile />
->>>>>>> eff078b (Agregar componente UploadFile en la aplicación)
     </>
-  );
-};
+  )
+}
+
+
