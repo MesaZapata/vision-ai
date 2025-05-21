a33f24ed9aa7faafe9aaaff726c4a250afc3ae1f
Samuel Mesa Zapata <mesazapatasamuel@gmail.com>
2025-05-20 20:37:03 -0500

    Agregar botón "Vision Student" en la interfaz de usuario y mejorar la estructura del componente App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { useState } from 'react';
-import './App.css'
-import { UploadFile } from './modules/vision'
-import { ThemeProvider } from './components/ui/themeProvider';
-import { ModeToggle } from './components/ui/modeToggle';
-import { ImageCaptioning } from './modules/vision/infrastructure/ImageCaptioning';
-import { Hero } from './modules/vision/ui/Hero';
+import { useState } from "react";
+import "./App.css";
+import { UploadFile } from "./modules/vision";
+import { ThemeProvider } from "./components/ui/themeProvider";
+import { ModeToggle } from "./components/ui/modeToggle";
+import { ImageCaptioning } from "./modules/vision/infrastructure/ImageCaptioning";
+import { Hero } from "./modules/vision/ui/Hero";
+import { Button } from "./components/ui/button";
 
 export const App = () => {
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
@@ -12,20 +13,28 @@ export const App = () => {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <div className="fixed top-4 right-4 z-10">
+        <Button
+          variant="outline"
+          className="mr-2"
+          onClick={() => {
+            window.open("https://vision-grade.vercel.app", "_blank");
+          }}
+        >
+          Vision Student
+        </Button>
         <ModeToggle />
       </div>
-      
+
       <Hero />
-      
+
       <div className="container mx-auto px-4 -mt-4">
         <div className="max-w-2xl mx-auto">
           <UploadFile onFileChange={(file) => setSelectedFile(file)} />
           <div className="flex justify-center items-center">
-            <ImageCaptioning file={selectedFile}/>
+            <ImageCaptioning file={selectedFile} />
           </div>
         </div>
       </div>
     </ThemeProvider>
-  )
-}
-
+  );
+};
